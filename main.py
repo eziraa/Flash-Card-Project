9996ca4ec66e640c08ca649f8d8462e0c0878c3c
sutual <ezirayallew@gmail.com>
2023-09-09 12:29:53 +0300

    Image and title text added

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from tkinter import *
-GREEN = "#B1DDC7"
+
+GREEN = "#9fd6b8"
 # Creating the main window
 window = Tk()
 window.title("Flashy")
-window.config(pady=20, padx=20,bg=GREEN)
+window.config(pady=20, padx=20, bg=GREEN)
+canvas = Canvas(width=867, height=568)
+card_front_img = PhotoImage(file="Images/card-front.png")
+canvas.create_image(433.5, 279, image=card_front_img)
+canvas.create_text(433.5, 180, text="Title", font=("Arial", 40, "italic"))
+canvas.grid(row=0, column=0)
+canvas.config(bg=GREEN,highlightthickness=0)
 
 window.mainloop()
